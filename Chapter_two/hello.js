f3d3c5d75f68d8e232dfc7a481e1eed9f7e7e0b8
MD-RAKIBL-ISLAM <md.rakibul2828@gmail.com>
2023-11-18 20:15:50 +0600

    updated

---
+++ Chapter_two/hello.js
@@ -0,0 +1,8 @@
+var myname = 'Md.Rakibul Islam'
+var age = 22
+ 
+console.log(myname+" knows Javascript")
+console.log("His age is "+age)
+console.log(myname+" is creating a Javascript for beginners")
+console.log("But his age is only "+age) 
+
